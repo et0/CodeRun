ed3ae5cb66c7cf99c2df1372baf3870e02d896c9
Lenya Nab <nabiev.lenya@gmail.com>
2025-09-15 19:21:51 +0400

    Add solution and tests for 0128

---
+++ middle/0128/solution.go
@@ -0,0 +1,65 @@
+package main // https://coderun.yandex.ru/problem/pedigree-number-of-descendants
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	var in *bufio.Reader
+	var out *bufio.Writer
+	in = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	var children, parent string
+	nodes := make(map[string]int, n)
+	relations := make(map[string][]string, n-1)
+	for range n - 1 {
+		fmt.Fscan(in, &children, &parent)
+
+		nodes[children] = 0
+
+		relations[parent] = append(relations[parent], children)
+	}
+
+	// ищем корень дерева - родитель, которого нет в общем списке (nodes)
+	var root string
+	for p := range relations {
+		if _, ok := nodes[p]; !ok {
+			root = p
+			break
+		}
+	}
+
+	setCountChildren(root, &nodes, &relations)
+
+	result := make([]string, 0, n)
+	for k, v := range nodes {
+		result = append(result, k+" "+strconv.Itoa(v))
+	}
+
+	sort.Strings(result)
+
+	for _, v := range result {
+		fmt.Fprintln(out, v)
+	}
+}
+
+func setCountChildren(parent string, nodes *map[string]int, relations *map[string][]string) int {
+	counter := 0
+
+	for _, children := range (*relations)[parent] {
+		counter += 1 + setCountChildren(children, nodes, relations)
+	}
+
+	(*nodes)[parent] = counter
+
+	return counter
+}
